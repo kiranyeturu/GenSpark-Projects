35b2147588a18a39a1f1eea7be993dac0bcc0bf6
Kiran <kiranmayeemettu@gmail.com>
2022-03-12 18:40:08 -0500

    updated

--- DragonCave-Project1/src/com/example/dragoncave/DragonCave.java
+++ DragonCave-Project1/src/com/example/dragoncave/DragonCave.java
@@ -1,5 +1,6 @@
 package com.example.dragoncave;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DragonCave {
@@ -10,22 +11,34 @@ public class DragonCave {
         System.out.println("you see two caves. In one cave, the dragon is friendly");
         System.out.println("and will share his treasure with you. The other dragon");
         System.out.println("is greedy and hungry and will eat you on sight.");
-        System.out.println("Which cave will you go into?(1 or 2)");
 
-        int userInput = sc.nextInt();
-        if(userInput == 1){
-            System.out.println("You approach the cave...");
-            System.out.println("It is dark and spooky...");
-            System.out.println("A large dragon jumps out in front of you! He opens his jaws and ...");
-            System.out.println("Gobbles you down in one bite!");
-        }
-        else if (userInput == 2){
-            System.out.println("the dragon wants to say Hi!!");
-        }
-        else{
-            System.out.println("Please enter a value 1 or 2 !");
-        }
 
+        int gamerInput;
+        Scanner in = new Scanner(System.in);
+
+        //int userInput = sc.nextInt();
+
+        try
+        {
+            System.out.println("Which cave will you go into?(1 or 2)");
+            gamerInput = in.nextInt();
+            if(gamerInput == 1){
+                System.out.println("You approach the cave...");
+                System.out.println("It is dark and spooky...");
+                System.out.println("A large dragon jumps out in front of you! He opens his jaws and ...");
+                System.out.println("Gobbles you down in one bite!");
+            }
+            else if (gamerInput == 2){
+                System.out.println("the dragon wants to say Hi!!");
+            }
+            else{
+                System.out.println("Please enter a value 1 or 2 !");
+            }
+        }
+        catch (InputMismatchException e)
+        {
+            System.out.print("Please Enter Valid Numbers.");
+        }
 
     }
 }
